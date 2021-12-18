802b858a704f9fde52aa193b69f60472ad32750b
blacksempai <benjaminfranclin1998@gmail.com>
2021-12-18 02:43:01 +0200

    Add orders functionality

--- admin.js
+++ admin.js
@@ -26,4 +26,38 @@ productForm.addEventListener('submit', function(event){
     xhr.send(data);
 })
 
+let orders = document.getElementById('admin_page_orders');
 
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://market-6d33.restdb.io/rest/orders");
+xhr.responseType = 'json'
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-apikey", "61b3ca4f72a03f5dae8222ad");
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.onload = function() {
+    xhr.response.forEach(function(order){
+        orders.innerHTML += `
+            <h2>Order ${order._id}</h2>
+            <p><b>Status:</b> ${order.status}</p>
+            <p><b>Customer name:</b> ${order.name}</p>
+            <p><b>Address:</b> ${order.address}</p>
+            <p><b>Phone:</b> ${order.phone}</p>
+            <p><b>Post Office Number:</b> ${order.post_number}</p>
+        `;  
+        let sum = 0;
+        order.products.forEach(function(p){
+            orders.innerHTML += `
+                <p><img height="50" src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+            `;
+            sum += +p.price;
+        });
+        orders.innerHTML += `
+        <p>Total Price: ${sum}$</p> 
+        <button>Mark as Completed</button> 
+        <hr>
+        `;
+    })
+}
+
+xhr.send();
